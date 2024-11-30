042823bf05b6493fc6e7472c28f4e7183c69c0c1
poo0414 <180311814+masterAI359@users.noreply.github.com>
2024-11-29 18:36:33 -0600

    adding a map of thought process

---
+++ client/src/components/React/Map/MapContainer.tsx
@@ -0,0 +1,10 @@
+//TODO: Implement the argument/mind map side by side with the InvestigateHero component. We need 
+// to make this entire process feel more intuitive upon first glance
+
+
+export default function MapContainer() {
+
+    return (
+        <section></section>
+    )
+}

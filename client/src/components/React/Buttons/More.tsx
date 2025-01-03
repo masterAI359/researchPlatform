c3547455feed561079669ab8444780d016a479f9
poo0414 <180311814+masterAI359@users.noreply.github.com>
2025-01-03 16:09:50 -0600

    mobile view css

--- client/src/components/React/Buttons/More.tsx
+++ client/src/components/React/Buttons/More.tsx
@@ -4,21 +4,22 @@
 export default function More({ article_url, setOpen }) {
 
     return (
-        <div className="bg-black border border-white/40 rounded-md xl:w-40 h-auto p-5 absolute md:right-12">
+        <div className="bg-black border border-white/40 z-30 rounded-md xs:w-36 xl:w-40 h-auto p-5 absolute xs:right-12">
             <div
                 onClick={() => { setOpen(false) }}
-                className="absolute right-1 top-1 hover:bg-white/10 rounded-md p-1 md:h-8 md:w-8 cursor-pointer">
+                className="absolute right-1 top-1 hover:bg-white/10 rounded-md p-1 xs:h-7 xs:w-7 md:h-8 md:w-8 cursor-pointer">
                 <svg className="text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="100%" height="100%">
                     <path d="M 39.486328 6.9785156 A 1.50015 1.50015 0 0 0 38.439453 7.4394531 L 24 21.878906 L 9.5605469 7.4394531 A 1.50015 1.50015 0 0 0 8.484375 6.984375 A 1.50015 1.50015 0 0 0 7.4394531 9.5605469 L 21.878906 24 L 7.4394531 38.439453 A 1.50015 1.50015 0 1 0 9.5605469 40.560547 L 24 26.121094 L 38.439453 40.560547 A 1.50015 1.50015 0 1 0 40.560547 38.439453 L 26.121094 24 L 40.560547 9.5605469 A 1.50015 1.50015 0 0 0 39.486328 6.9785156 z" fill="currentColor" />
                 </svg>
             </div>
             <div className="w-full mx-auto flex flex-col gap-y-2">
-                <div className="text-white text-left hover:text-blue-400 transition-all duration-200 ease-in-out md:text-md font-light tracking-tight">
-                    <a href={article_url}>
+                <div className="text-white text-left hover:text-blue-400 transition-all cursor-pointer 
+                duration-200 ease-in-out xs:text-xs md:text-md font-light tracking-tight">
+                    <a href={article_url} target="_blank">
                         Visit source
                     </a>
                 </div>
-                <div className="text-white text-left hover:text-blue-400 transition-all duration-200 ease-in-out md:text-md font-light tracking-tight">
+                <div className="text-white text-left xs:text-xs hover:text-blue-400 transition-all cursor-pointer duration-200 ease-in-out md:text-md font-light tracking-tight">
                     <a href="#">
                         All authors
                     </a>
--- client/src/components/React/Buttons/MoreButton.tsx
+++ client/src/components/React/Buttons/MoreButton.tsx
@@ -7,11 +7,14 @@ import More from "./More"
 export default function MoreButton({ article_url }) {
     const [open, setOpen] = useState<boolean>(false)
 
+
+    console.log(open)
+
     return (
-        <div className='w-fit h-fit xs:self-start md:self-end xl:mr-2 relative'>
+        <div className='w-fit h-fit xs:self-start md:self-end xl:mr-2 xs:mt-2 md:mt-0 relative'>
             {open && <More key={article_url} article_url={article_url} setOpen={setOpen} />}
             <div className="group relative">
-                {!open && <div className="rounded-md xl:h-fit xl:w-16 flex mx-auto bg:black/80 opacity-0 absolute xl:-translate-y-9 xl:-translate-x-8
+                {!open && <div className="rounded-md xl:h-fit xl:w-16 flex xs:hidden md:block mx-auto bg:black/80 opacity-0 absolute xl:-translate-y-9 xl:-translate-x-8
 border border-white/50 md:group-hover:opacity-100 transition-opacity duration-200 ease-in-out">
 
                     <h1 className="text-white xl:text-sm xl:p-1 text-center font-light tracking-tight justify-self-start text-center w-full">
@@ -19,9 +22,9 @@ border border-white/50 md:group-hover:opacity-100 transition-opacity duration-20
                     </h1>
                 </div>}
                 <div
-                    onClick={() => { setOpen(prev => !prev) }}
+                    onMouseUp={() => { setOpen(prev => !prev) }}
                     className='box-border flex items-center hover:bg-white/10 transition-all duration-200 
-            ease-in-out xs:py-0.5 xs:px-2 rounded-xl'>
+            ease-in-out xs:py-1 xs:px-2 rounded-xl cursor-pointer'>
                     <button
                         className='flex items-center justify-center text-white xs:text-xs md:text-sm 
                     transition-all duration-200 ease-in-out my-auto xs:rounded-full md:rounded-full xs:w-5 lg:w-6 h-full'

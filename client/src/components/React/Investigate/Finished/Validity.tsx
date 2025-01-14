badca8b145c350d837bdd2cc1fec79f4926ed949
poo0414 <180311814+masterAI359@users.noreply.github.com>
2025-01-14 13:57:21 -0600

    changed note svg

--- client/src/components/React/Investigate/Finished/Validity.tsx
+++ client/src/components/React/Investigate/Finished/Validity.tsx
@@ -1,9 +1,13 @@
-
+import { useSelector, useDispatch } from "react-redux"
+import { RootState } from "@/ReduxToolKit/store"
 //TODO: create component to gauge whether the premises held up under scrutiny or not
 
 export default function Validity() {
+    const premises = useSelector((state: RootState) => state.pov.premises)
 
     return (
-        <div></div>
+        <div>
+            {premises}
+        </div>
     )
 }

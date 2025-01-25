46acc9b7efe52ed56c683ab715f852e2fe3e973a
poo0414 <180311814+masterAI359@users.noreply.github.com>
2025-01-25 15:14:37 -0600

    loading animation change

--- client/src/components/React/Loaders/SummaryLoader.tsx
+++ client/src/components/React/Loaders/SummaryLoader.tsx
@@ -1,25 +1,26 @@
 import Lottie from "lottie-react"
-import squareCircle from '../../../lotties/SquareCircle.json'
+//import squareCircle from '../../../lotties/SquareCircle.json'
+import dataCollection from '../../../lotties/dataCollection.json'
 import { createPortal } from "react-dom"
 
 export default function SummaryLoader() {
 
 
     const loader = (
-        <div className='w-full h-full mx-auto grid grid-cols-1 justify-center absolute bg-black top-0 right-0 bottom-0 left-0'>
+        <div className='w-full h-full mx-auto flex flex-col items-center absolute bg-black top-0 right-0 bottom-0 left-0'>
 
-            <header className='w-full mx-auto mt-24'>
+            <header className='w-full h-fit mx-auto xl:translate-y-16'>
                 <h1
-                    className='text-center font-serif text-4xl text-slate-400'
+                    className='text-center font-light tracking-tight text-4xl text-white'
                 >Just a few moments while we gather those articles</h1>
             </header>
-            <div className="w-full h-fit mx-auto">
+            <div className="w-3/5 h-fit mx-auto pb-12 2xl:-translate-y-24">
                 <Lottie
-                    className="mx-auto"
-                    animationData={squareCircle}
+                    className=""
+                    animationData={dataCollection}
                     autoPlay={true}
                     loop={true}
-                    style={{ height: 400, width: 400 }}
+                    style={{ height: '100%', width: '100%' }}
                 />
             </div>
 

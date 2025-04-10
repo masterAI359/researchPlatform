60e7437b13e74d6b0c0263e66d1cea45412904c0
poo0414 <180311814+masterAI359@users.noreply.github.com>
2025-04-10 17:32:04 -0500

    forgot to add link to mobile profile nav for acc management

--- client/src/components/React/User/ProfileNavigation/ProfileMenu.tsx
+++ client/src/components/React/User/ProfileNavigation/ProfileMenu.tsx
@@ -1,10 +1,7 @@
 import { RootState } from "@/ReduxToolKit/store"
 import { useSelector, useDispatch } from "react-redux"
-import { AppDispatch } from "@/ReduxToolKit/store"
-import { presentArticles, presentResearch } from "@/ReduxToolKit/Reducers/UserContent.ts/ProfileNavigationSlice";
-import { Link } from "react-router-dom";
+import { presentArticles, presentResearch, presentManagement } from "@/ReduxToolKit/Reducers/UserContent.ts/ProfileNavigationSlice";
 import { showSignOut } from "@/ReduxToolKit/Reducers/Athentication/Authentication";
-
 
 
 export default function MobileProfileNav () {
@@ -37,7 +34,13 @@ export default function MobileProfileNav () {
       </svg>
       <span className="text-xs text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500 transition-all duration-200 ease-in-out">Articles</span>
     </button>
-    <button type="button" className="inline-flex flex-col items-center justify-center px-5  group">
+    <button 
+    onClick={() => {
+    dispatch(presentManagement(true))
+    dispatch(presentArticles(false))
+    dispatch(presentResearch(false))
+    }}
+    type="button" className="inline-flex flex-col items-center justify-center px-5  group">
       <svg className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
       </svg>

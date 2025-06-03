d96db4762baa34e6dcf5aedcbe8693b121a84c6a
poo0414 <180311814+masterAI359@users.noreply.github.com>
2025-06-03 17:23:39 -0500

    UI for media bias ratings on articles

--- client/src/components/React/Articles/SuccessFull/ArticleHeader.tsx
+++ client/src/components/React/Articles/SuccessFull/ArticleHeader.tsx
@@ -93,22 +93,34 @@ export default function ArticleHeader({ articleData, setFullStory, fullStory }) 
                         <div
                             style={{ backgroundImage: `url("${storyImage}")` }}
                             className='absolute inset-0 w-full h-full xl:w-full
-                            bg-cover bg-center opacity-60 xs:rounded-xl xl:rounded-lg'
+                            bg-cover bg-center opacity-40 xs:rounded-xl xl:rounded-lg'
                         >
                         </div>
-                        <div className='relative z-10 xs:p-2 flex flex-col gap-y-10 xs:py-6 md:py-6'>
+                        <div className='relative z-10 xs:p-2 flex flex-col gap-y-3 xs:py-6 md:py-6'>
 
                             <div className="w-auto h-full">
                                 <h1 className='lg:text-lg xs:text-md leading-6 text-white font-light tracking-tight font-serif'>
                                     {article_title}
                                 </h1>
                             </div>
+
                             <div className="flex items-center self-start">
-                                <p className="text-white opacity-100 xs:text-xs md:text-md flex items-center">
+                                <p className="text-white/70 opacity-100 xs:text-xs md:text-md flex items-center">
                                     {logo && <img className="mr-3 h-9 w-9" src={logo} alt={''} />}
 
                                     {source}
                                 </p>
+                            </div>
+                            <div id="source_bias_ratings" className="flex gap-x-2 items-center justify-start w-auto h-auto flex-nowrap">
+                                <div className="w-9 h-9 flex items-center justify-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width={'100%'} height={'100%'} viewBox="0 0 24 24" fill="currentColor"
+                                        className="text-white icon icon-tabler icons-tabler-filled icon-tabler-compass"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M17 3.34a10 10 0 1 1 -15 8.66l.005 -.324a10 10 0 0 1 14.995 -8.336zm-5 14.66a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm3.684 -10.949l-6 2a1 1 0 0 0 -.633 .633l-2.007 6.026l-.023 .086l-.017 .113l-.004 .068v.044l.009 .111l.012 .07l.04 .144l.045 .1l.054 .095l.064 .09l.069 .075l.084 .074l.098 .07l.1 .054l.078 .033l.105 .033l.109 .02l.043 .005l.068 .004h.044l.111 -.009l.07 -.012l.02 -.006l.019 -.002l.074 -.022l6 -2a1 1 0 0 0 .633 -.633l2 -6a1 1 0 0 0 -1.265 -1.265zm-1.265 2.529l-1.21 3.629l-3.629 1.21l1.21 -3.629l3.629 -1.21zm-9.419 1.42a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm14 0a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm-7 -7a1 1 0 1 0 0 2a1 1 0 0 0 0 -2z" /></svg>
+                                </div>
+                                <div id="bias_rating bg-black/60 px-2 py-1 rounded-md text-sm">
+                                    <p className="text-white/70 font-light text-sm md:text-md">
+                                        Source bias: <span className="text-white/60">{rating}</span>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -116,25 +128,25 @@ export default function ArticleHeader({ articleData, setFullStory, fullStory }) 
                 <article className="w-full h-full flex items-center justify-between self-end pt-4 md:pt-0">
                     <figcaption className="w-auto md:w-full h-full flex items-baseline self-start md:self-end">
                         <div className='flex md:flex-col h-full w-full box-border xl:gap-y-4 self-end'>
-                            <div className='w-full h-full box-border flex flex-col md:gap-y-4 justify-end'>
+                            <div className='w-full h-full box-border flex flex-col md:gap-y-2 justify-end'>
 
                                 <div>
-                                    <p className="text-white font-light xs:text-sm md:text-lg font-serif">
-                                        Published - <span className="text-slate-400 font-light xs:text-sm md:text-lg font-serif">
+                                    <p className="text-white font-light xs:text-sm md:text-lg">
+                                        Published - <span className="text-slate-300 font-light xs:text-sm md:text-lg">
                                             {date ? dateFormatted : article_pub_date}{' '}
                                         </span>
                                     </p>
                                 </div>
-                                <div className='max-w-96 flex flex-wrap mt-3 items-center'>
-                                    <p className='text-white text-sm md:text-lg mr-2'>Authors - </p>
+                                <div className='max-w-96 flex flex-wrap items-center'>
+                                    <p className='text-white font-light text-sm md:text-lg mr-2'>Authors - </p>
                                     {article_authors && showAllAuthors === false && authShortened.map((author: string, index: number) => {
 
                                         if (index + 1 < authShortened.length) {
-                                            return (<p key={index} className="text-slate-400 md:text-lg text-sm font-serif mr-2">
+                                            return (<p key={index} className="text-slate-300 md:text-lg text-sm font-light mr-2">
                                                 {author},
                                             </p>)
                                         } else if (index + 1 === authShortened.length) {
-                                            return (<p key={index} className="text-slate-400 md:text-lg text-sm font-serif mr-2">
+                                            return (<p key={index} className="text-slate-300 md:text-lg text-sm font-light mr-2">
                                                 {author}
                                             </p>)
                                         }
@@ -142,12 +154,12 @@ export default function ArticleHeader({ articleData, setFullStory, fullStory }) 
                                     {article_authors && showAllAuthors === true && article_authors.map((author: string, index: number) => {
 
                                         if (index + 1 < article_authors.length) {
-                                            return (<p key={index} className="text-slate-400 md:text-lg font-serif mr-2">
+                                            return (<p key={index} className="text-slate-400 md:text-lg font-light mr-2">
                                                 {author},
                                             </p>)
                                         } else if (index + 1 === article_authors.length) {
                                             return (
-                                                <p key={index} className="text-slate-400 md:text-lg font-serif mr-2">
+                                                <p key={index} className="text-slate-400 md:text-lg font-light mr-2">
                                                     {author}
                                                 </p>
                                             )
@@ -155,6 +167,8 @@ export default function ArticleHeader({ articleData, setFullStory, fullStory }) 
                                     })}
                                     {article_authors === null && (<p className="text-slate-400 font-serif">Authors could not be determined. Visit the source to view the article's authors.</p>)}
                                 </div>
+
+
                             </div>
                         </div>
                     </figcaption>

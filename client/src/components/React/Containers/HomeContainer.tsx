aea8068ff73aa08d4817a839b7e83c040987a7ec
poo0414 <180311814+masterAI359@users.noreply.github.com>
2025-06-16 16:14:52 -0500

    dependency array fix

--- client/src/components/React/Containers/HomeContainer.tsx
+++ client/src/components/React/Containers/HomeContainer.tsx
@@ -28,7 +28,7 @@ export default function HomeContainer({ }) {
 
         ScrollUp();
 
-    }, [posts, blueSkyStatus]);
+    }, []);
 
     return (
         <section className={`flex h-auto flex-col w-full grow transition-opacity duration-200 delay-200 ease-in-out scroll-smooth thin-gray-scrollbar

ece4b95d8c3ec631505fba9e7729b02a8cc5f6f0
poo0414 <180311814+masterAI359@users.noreply.github.com>
2025-07-16 11:20:34 -0500

    typo caught in header text - step 1 header

--- client/src/components/React/Investigate/Steps/StepHeader.tsx
+++ client/src/components/React/Investigate/Steps/StepHeader.tsx
@@ -18,9 +18,8 @@ export default function StepHeader({ title, subheader, info }) {
                 <div className="w-full flex justify-items-start flex-nowrap">
                     <h1 className="2xl:text-2xl md:text-2xl sm:text-xl text-sm 
                      tracking-tight font-light text-nowrap text-zinc-300 pb-1"
-                    >{title}
-                        <span className="text-zinc-500"
-                        >{subheader ? subheader : null}
+                    >{title} <span className="text-zinc-500"
+                    >{subheader ? subheader : null}
                         </span>
                     </h1>
                 </div>

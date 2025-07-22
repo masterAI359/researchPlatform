985dd54ac00aabea909b8c3d52437abaa781f21f
poo0414 <180311814+masterAI359@users.noreply.github.com>
2025-07-22 11:33:57 -0500

    file paths mended for case sensitivity

--- client/src/components/React/features/investigate/Containers/InvestigationWorkSpace.tsx
+++ client/src/components/React/features/investigate/Containers/InvestigationWorkSpace.tsx
@@ -1,10 +1,10 @@
 import { RootState } from '@/ReduxToolKit/store';
 import { lazy, Suspense, useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
-import ComponentLoader from '../../../Loaders/ComponentLoader';
+import ComponentLoader from '../../../Shared/Loaders/ComponentLoader';
 import { AnimatePresence } from 'framer-motion';
 import InputOptions from '../GetPerspective/components/paths/InputOptions';
-const BlueSkyPosts = lazy(() => import('../../BlueSky/Containers/BlueSkyPosts'));
+const BlueSkyPosts = lazy(() => import('../../blueSky/Containers/BlueSkyPosts'));
 import HeroContainer from './HeroContainer';
 const Content = lazy(() => import('./Content'));
 import Notes from '../notes/Notes';

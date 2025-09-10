a64da1fb73e99c5b0a36cdc771d70c57a0dfd308
poo0414 <180311814+masterAI359@users.noreply.github.com>
2025-09-10 13:39:03 -0500

    casing change to sync git list of directories for landing page buttons

--- client/src/components/React/features/LandingPage/buttons/LearnMore.tsx
+++ client/src/components/React/features/LandingPage/buttons/LearnMore.tsx
@@ -6,13 +6,16 @@ export default function LearnMore() {
 
 
     return (
-        <div className="text-xs sm:text-sm py-2 px-4 border focus:ring-2 rounded-full border-transparent w-44 lg:w-36 
+        <Link to='/about'>
+            <div className="text-xs sm:text-sm py-2 px-4 border focus:ring-2 rounded-full border-transparent w-44 lg:w-36 
         bg-white hover:bg-white/10 text-black duration-200 focus:ring-offset-2 focus:ring-white 
         hover:text-white flex items-center flex-nowrap justify-center ring-1 ring-transparent cursor-pointer">
 
-            <Link to='/About'><div className="flex text-nowrap">Learn More &nbsp; →</div>  </Link>
+                <div className="flex text-nowrap">Learn More &nbsp; →</div>
 
-        </div>
+            </div>
+        </Link>
+
 
     )
 }
